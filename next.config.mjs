a8d80134d3a68355ece6cc483977f970389d8a10
Bayu Aditiya <bayuadi912@gmail.com>
2025-08-04 23:27:31 +0700

    updated version

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "qhlgvfbcyomfxbcrgclw.supabase.co",
+        hostname: "iaoqudjdonvkzrxvxeqd.supabase.co",
         port: "",
         pathname: "/storage/v1/object/public/cabin-images/**",
         search: "",
